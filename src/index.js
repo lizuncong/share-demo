d92b914f582500578f08fbc4f2d7e9e2f2cf07e3
lizuncong <2900170896@qq.com>
2020-09-11 14:05:31 +0800

    demo01

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 var express = require('express')
 
+
+/**
+ * 服务器端渲染的原始方式，因此就有了ejs，pug等node服务端模版引擎
+ * */
 var app = express()
 
-app.get('/', function(req, res){
+app.get('/helloword', function (req, res) {
     res.send('hello word')
+})
+app.get('/', function(req, res){
+    res.send(
+        `<html>
+            <head>
+                <meta charSet="utf-8" />
+                <title>demo1</title>
+                <meta name="description" content="这是一个很好的网站" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </head>  
+            <body>
+                <div>这是一段HTML</div>
+            </body>
+        </html>`
+    )
 })
 
 var server = app.listen(3000, function(){
