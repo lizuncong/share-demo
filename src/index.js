6a187b3c0f2b241e0bcd50c2c5f2e3016727c48e
lizuncong <2900170896@qq.com>
2021-01-21 23:59:44 +0800

    stash

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const path = require("path");
+const React = require('react';
+const express = require('express')
 
-var express = require('express')
+const app = express()
 
-
-/**
- * 服务器端渲染的原始方式，因此就有了ejs，pug等node服务端模版引擎
- * */
-var app = express()
-
-app.use(express.static(path.resolve(__dirname, 'js')));
+const Counter = ({ count }) => {
+    return (
+        <div>{count}</div>
+    )
+}
 
 app.get('/', function(req, res){
     const data = { count: 1 }
+
     res.send(
         `<html>
             <head>
@@ -30,33 +30,7 @@ app.get('/', function(req, res){
     )
 })
 
-/* 可以响应点击事件的demo */
-app.get('/js', function(req, res){
-    const data = { count: 1 }
-    res.send(
-        `<html>
-            <head>
-                <meta charSet="utf-8" />
-                <title>demo1</title>
-                <meta name="description" content="这是一个很好的网站" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </head>  
-            <body>
-                <div>
-                这是一段可以响应点击事件的HTML
-                <div>
-                    计数器：
-                    <div id="myCount">${data.count}</div>
-                </div>
-                </div>
-                <script src="/click.js" type="text/javascript"></script>
-            </body>
-        </html>`
-    )
-})
 
-var server = app.listen(3000, function(){
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('server listening at : localhost:' + port)
+app.listen(3000, function(){
+    console.log('server listening at : localhost:3000')
 })
--- src/js/click.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-    const countEl = document.getElementById('myCount');
-    countEl.addEventListener("click", function(){
-        countEl.innerHTML = Number(countEl.innerHTML) + 1;
-    })
-}
