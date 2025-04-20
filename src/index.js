9c6c8bb857f97babac8f3a50b05a4c98dc3bd2cc
lizuncong <2900170896@qq.com>
2025-04-20 15:29:58 +0800

    feat: demo1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-const React = require('react';
 const express = require('express')
 
 const app = express()
 
-const Counter = ({ count }) => {
-    return (
-        <div>{count}</div>
-    )
-}
-
 app.get('/', function(req, res){
     const data = { count: 1 }
-
     res.send(
         `<html>
             <head>
@@ -19,12 +11,26 @@ app.get('/', function(req, res){
                 <title>demo1</title>
                 <meta name="description" content="这是一个很好的网站" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <style>
+                    #counter {
+                        cursor: pointer;
+                        color: red;
+                    }
+                </style>
             </head>  
             <body>
                 <div>
-                这是一段不能响应点击的HTML
-                <div>计数器：${data.count}</div>
+                这是一个服务端渲染返回的页面：
+                <div id="counter">计数器：${data.count}</div>
                 </div>
+                <script>
+                    let data = {count: 1}
+                    document.getElementById('counter').addEventListener('click', function(){
+                        data.count = data.count + 1
+                        counter.innerHTML = '计数器：' + data.count
+                        
+                    })
+                </script>
             </body>
         </html>`
     )
